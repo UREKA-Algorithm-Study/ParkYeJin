084c514b77750fa52c5778b5425f99afa9bb4b47
parkyejin <uiop5809@naver.com>
2024-07-16 16:44:44 +0900

    [CDT] 마법의 숲 탐색 30% / 삼성 기출 2024

--- Codetree/시뮬레이션/마법의 숲 탐색.cpp
+++ Codetree/시뮬레이션/마법의 숲 탐색.cpp
@@ -3,7 +3,6 @@
 #include <queue>
 using namespace std;
 
-int Tcnt = 0;
 int R, C, K; 
 
 int dx[4] = {1, 0, -1, 0};
@@ -11,7 +10,6 @@ int dy[4] = {0, -1, 0, 1};
 
 int arr[71][71];
 int newArr[71][71];
-int nowX, nowY, nowDir; // 현재 골룸 위치
 
 int westDx[5] = {-1, 0, 1, 1, 2};
 int westDy[5] = {-1, -2, -2, -1, -1}; 
@@ -22,53 +20,51 @@ int eastDy[5] = {1, 2, 1, 2, 1};
 int visited[71][71];
 bool flag = false;
 
-void reset() {
+void mapReset() {
     memset(arr, 0, sizeof(arr));
     memset(newArr, 0, sizeof(newArr));
-    Tcnt = 0;
     flag = true;
+}
+
+void blockReset(int x, int y){
+    arr[x][y] = 0;
+    for(int i = 0; i < 4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        arr[nx][ny] = 0;
+    }
 }
 
 int downHeight(int x, int y) {
     int cnt = 0;
     for(int i = x + 1; i <= R; i++){
         int left = arr[i][y - 1];
-        int g = arr[i][y];
+        int center = arr[i][y];
         int right = arr[i][y + 1];
 
-        if (!left && !g && !right) cnt++;
+        if (!left && !center && !right) cnt++;
         // 블록이 있을 경우 
         else {
-            // 꼭대기 떨
-            if (g == 1) return i - (x + 1) - 1;
-            // 양쪽 떨
-            else return i - (x + 1);
+            if (center == 1) return i - (x + 1) - 1; // 꼭대기 떨
+            else return i - (x + 1); // 양쪽 떨
         }
     }
-    // 블록이 없을 경우 (처음)
-    if (cnt == R + 1) return R;
-    // 블록이 없을 경우 (땅바닥)
-    else return 0; 
+    // 블록이 없을 경우
+    if (cnt == R - x) return R - x - 1;
+    else return 0;
 }
 
-void down(int x, int y, int h, int dir) {
-    if (flag) return;
-
+void down(int &x, int &y, int h, int &dir){
+    if (flag) return; // reset 상태면 패스
     x += h; // 떨어진 골렘 위치
-    cout << "떨어진 골렘 위치 " << x << " " << y << " " << h << "\n";
-
-    nowX = x;
-    nowY = y;
-    nowDir = dir;
+    
+    // 현재 골렘 블록 표시 
     arr[x][y] = 1;
-
     for(int i = 0; i < 4; i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
         arr[nx][ny] = 1;
     }
-
-    if (h == R) return;
 
     // 서쪽 회전이 가능한지 확인
     int wcnt = 0;
@@ -77,27 +73,18 @@ void down(int x, int y, int h, int dir) {
         int ny = y + westDy[i];
 
         if (nx > R || ny < 1 || ny > C) continue;
-        // 5곳 다 0이어야 회전 가능
         if (!arr[nx][ny]) wcnt++;
     }
     if (wcnt == 5) {
-        // 원래 위치 0으로
-        arr[x][y] = 0;
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            arr[nx][ny] = 0;
-        }
+        blockReset(x, y); // 원래 블록 위치 0으로
 
-        // 방향 바꾸기
         x += 1;
         y -= 1;
         dir -= 1;
         if (dir < 0) dir = 3;
 
-        // 또 떨어트리기
-        int h = downHeight(x, y);
-        down(x, y, h, dir);
+        down(x, y, downHeight(x, y), dir); // 남쪽 재귀
+        return;
     }
 
     // 동쪽 회전이 가능한지 확인
@@ -107,39 +94,29 @@ void down(int x, int y, int h, int dir) {
         int ny = y + eastDy[i];
 
         if (nx > R || ny < 1 || ny > C) continue;
-        // 4곳 다 0이어야 회전 가능
         if (!arr[nx][ny]) ecnt++;
     }
     if (ecnt == 5) {
-        // 원래 위치 0으로
-        arr[x][y] = 0;
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            arr[nx][ny] = 0;
-        }
+        blockReset(x, y);
 
-        // 방향 바꾸기
         x += 1;
         y += 1;
         dir += 1;
         if (dir > 3) dir = 0;
 
-        // 떨어트리기
-        int h = downHeight(x, y);
-        down(x, y, h, dir);
+        down(x, y, downHeight(x, y), dir); // 남쪽 재귀
+        return;
     }
 
     // 가장 떨어졌을 때 떨어진 골렘 위치가 밖이지만 회전 못할 경우 reset
-    if(nowX <= 1) {
-        reset();
-        return;
-    }
+    if(x <= 1) mapReset();
+    return;
 }
 
 int bfs(int x, int y){
     memset(visited, 0, sizeof(visited));
-    queue <pair <int ,int >> q;
+    queue <pair <int, int>> q;
+
     q.push({x, y});
     visited[x][y] = 1;
     int cnt = 0;
@@ -177,14 +154,14 @@ int main() {
     cin >> R >> C >> K;
     int sum = 0;
     for(int i = 0; i < K; i++){
-        int c, d; // 골렘 출발 열, 출구 방향 정보
-        cin >> c >> d;
-        cout << c << " " << d << "\n";
         flag = false; 
+        int col, dir; // 골렘 출발 열, 출구 방향 정보
+        cin >> col >> dir;
+        cout << col << " " << dir << "\n";
 
-        int h = downHeight(-1, c);
-        cout << "떨어트리는 높이 " << h << "\n";
-        down(-1, c, h, d);
+        int x = -1, y = col; // 현재 골룸의 위치
+        int height = downHeight(x, y); // 골렘 떨어지는 높이
+        down(x, y, height, dir); 
 
         // arr 테스트
         cout << "arr\n";
@@ -193,64 +170,40 @@ int main() {
                 cout << arr[i][j];
             }
             cout << "\n";
-        }   
-
-        if (flag) {
-            continue; // 맵 초과
         }
+
+        if (flag) continue; // reset이면 아무 동작 x
+
+        /* 골렘 탈출하기 */
 
         // 방향 블록 위치
         int blockX, blockY;
-        if (nowDir == 0) {
-            blockX = nowX - 1;
-            blockY = nowY;
-        } else if (nowDir == 1) {
-            blockX = nowX;
-            blockY = nowY + 1;
-        } else if (nowDir == 2) {
-            blockX = nowX + 1;
-            blockY = nowY;
-        } else if (nowDir == 3) {
-            blockX = nowX;
-            blockY = nowY - 1;
-        }
-
-        if (Tcnt == 0) { // 처음 들어왔을 때나, reset후 처음
-            for(int i = 1; i <= R; i++){
-                for(int j = 1; j<= C; j++){
-                    newArr[i][j] = arr[i][j];
-                }
-            }
-        } else { // 후에 들어왔을 땐 방향만 연결
-            newArr[blockX][blockY] = 1;
+        if (dir == 0) {
+            blockX = x - 1;
+            blockY = y;
+        } else if (dir == 1) {
+            blockX = x;
+            blockY = y + 1;
+        } else if (dir == 2) {
+            blockX = x + 1;
+            blockY = y;
+        } else if (dir == 3) {
+            blockX = x;
+            blockY = y - 1;
         }
 
         int blockCnt = bfs(blockX, blockY);
+        if (blockCnt == 1) {
+            sum += (x + 1); // 이어지지 않을 경우
+        } else sum += res(); // 원래 것과 이어질 경우
+        cout << sum << "\n\n";
 
-        // newArr 테스트
-        cout << "newArr\n";
         for(int i = 1; i <= R; i++){
             for(int j = 1; j <= C; j++){
-                cout << newArr[i][j];
-            }
-            cout << "\n";
-        }
-
-        // 이어지지 않을 경우 (방향 위치 블록 상하좌우 다 0일 경우)
-        if (blockCnt == 1) {
-            sum += (nowX + 1);
-        } // 원래 것과 이어질 경우
-        else sum += res();
-        cout << sum << "\n";
-
-        for(int i = 1; i <= R; i++){
-            for(int j = 1; j<= C; j++){
                 newArr[i][j] = arr[i][j];
             }
         }
-        cout << "\n\n";
-        Tcnt++;
     }
-    //cout << sum;
+    cout << sum;
     return 0;
 }

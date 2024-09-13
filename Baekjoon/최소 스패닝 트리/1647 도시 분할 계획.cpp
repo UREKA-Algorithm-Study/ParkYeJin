5c8e4325642e30834ad0a0070e013045b8ee8ce6
parkyejin <uiop5809@naver.com>
2024-09-13 19:09:34 +0900

    [BOJ] 도시 분할 계획 / 골드4

--- Baekjoon/최소 스패닝 트리/1647 도시 분할 계획.cpp
+++ Baekjoon/최소 스패닝 트리/1647 도시 분할 계획.cpp
@@ -1,3 +1,5 @@
+// 크루스칼 알고리즘
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -57,6 +59,66 @@ int main(){
   sort(v.begin(), v.end());
   kruskal();
   cout << result - maxCost << "\n";
+
+  return 0;
+}
+
+
+// 프림 알고리즘
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int N, M, result, maxCost;
+vector<pair<int, int>> edge[100001];
+bool visited[100001];
+
+priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+void prim(){
+  visited[1] = true;
+  for(int i = 0; i < edge[1].size(); i++){
+    int next = edge[1][i].first;
+    int cost = edge[1][i].second;
+    pq.push({cost, next});
+  }
+
+  while(!pq.empty()){
+    int cost = pq.top().first;
+    int cur = pq.top().second;
+    pq.pop();
+
+    if (!visited[cur]) {
+      visited[cur] = true;
+      result += cost;
+      maxCost = max(maxCost, cost);
+
+      for(int i = 0; i < edge[cur].size(); i++){
+        int next = edge[cur][i].first;
+        int nextcost = edge[cur][i].second;
+        if (!visited[next]) {
+          pq.push({nextcost, next});
+        }
+      }
+    }
+  }
+}
+
+int main(){
+  cin >> N >> M;
+
+  for(int i = 0; i < M; i++){
+    int from, to, cost;
+    cin >> from >> to >> cost;
+    edge[from].push_back({to, cost});
+    edge[to].push_back({from, cost});
+  }
+  prim();
+  cout << result - maxCost;
 
   return 0;
 }

d59cd30cf1019888e7c7bb07f2de3c7404d72468
parkyejin <uiop5809@naver.com>
2024-10-07 21:37:41 +0900

    [BOJ] 연구소 / 골드4

--- Baekjoon/BFS/14502 연구소.cpp
+++ Baekjoon/BFS/14502 연구소.cpp
@@ -1,30 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-/*
-  0 빈칸, 1 벽, 2 바이러스
-  세울 수 있는 벽 3개, 꼭 3개 세워야함
-*/
-
-int N, M, res = 0;
-int arr[9][9], arr_copy[9][9];
+int N, M, res;
+int arr[9][9];
+int copy_arr[9][9];
+bool visited[9][9];
 
 int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, -1, 0, 1};
+int dy[4] = {0, -1, 0, 1}; 
 
-vector <pair <int, int>> v;
-
-void bfs(){
+// 0 빈칸, 1 벽, 2 바이러스
+void bfs(int x, int y){
   queue <pair <int, int>> q;
-  // 바이러스 초기 위치
-  for(int i = 0; i < v.size(); i++){
-    q.push({v[i].first, v[i].second});
-  }
+  q.push({x, y});
+  visited[x][y] = true;
 
-  while(!q.empty()){
+  while(!q.empty()) {
     int x = q.front().first;
     int y = q.front().second;
     q.pop();
@@ -33,39 +24,49 @@ void bfs(){
       int nx = x + dx[i];
       int ny = y + dy[i];
 
-      if (nx < 1 || nx > N || ny < 1 || ny > M)
-        continue;
-      // 새로운 벽이 생긴 영역에서 바이러스 감염
-      if (arr_copy[nx][ny] == 0) {
-        arr_copy[nx][ny] = 2;
+      if (nx < 1 || nx > N || ny < 1 || ny > M) continue;
+      if (!visited[nx][ny] && copy_arr[nx][ny] == 0) {
         q.push({nx, ny});
+        copy_arr[nx][ny] = 2;
+        visited[nx][ny] = true;
       }
     }
   }
-
-  int cnt = 0;
-  for(int i = 1; i <= N; i++){
-    for(int j = 1; j <= M; j++){
-      if (arr_copy[i][j] == 0) cnt++;
-    }
-  }
-  res = max(res, cnt);
+  return;
 }
 
-void wall(int cnt) {
-  if (cnt == 3) { // 벽이 3개 됐을 때
+// 벽세우기
+void wall(int cnt){
+  if (cnt == 3) {
+    memset(visited, 0, sizeof(visited));
     for(int i = 1; i <= N; i++){
       for(int j = 1; j <= M; j++){
-        arr_copy[i][j] = arr[i][j];
+        copy_arr[i][j] = arr[i][j];
       }
     }
-    bfs();
+    // 2 바이러스로 다 채우기
+    for(int i = 1; i <= N; i++){
+      for(int j = 1; j <= M; j++){
+        if (!visited[i][j] && copy_arr[i][j] == 2) {
+          bfs(i, j);
+        }
+      }
+    }
+    // 0 안전영역 개수
+    int cnt = 0;
+    for(int i = 1; i <= N; i++){
+      for(int j = 1; j <= M; j++){
+        if (copy_arr[i][j] == 0) {
+          cnt++;
+        }
+      }
+    }
+    res = max(cnt, res);
     return;
   }
-  // 벽 세우기
   for(int i = 1; i <= N; i++){
     for(int j = 1; j <= M; j++){
-      if (arr[i][j] == 0) {
+      if (arr[i][j] == 0){
         arr[i][j] = 1;
         wall(cnt + 1);
         arr[i][j] = 0;
@@ -75,30 +76,18 @@ void wall(int cnt) {
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL), cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
 
   cin >> N >> M;
   for(int i = 1; i <= N; i++){
     for(int j = 1; j <= M; j++){
-      cin >> arr[i][j]; 
-      // 바이러스일 경우
-      if (arr[i][j] == 2) { 
-        v.push_back({i, j});
-      }
+      cin >> arr[i][j];
     }
   }
-  // 0인 부분들 모두 벽 세워보기
-  for(int i = 1; i <= N; i++){
-    for(int j = 1; j <= M; j++){
-      if (arr[i][j] == 0) {
-        arr[i][j] = 1; // 벽
-        wall(1);
-        arr[i][j] = 0;
-      }
-    }
-  }
-  cout << res;
   
+  wall(0);
+  cout << res;
+
   return 0;
-}
+} 

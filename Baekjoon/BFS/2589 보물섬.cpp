1a53247564e5140bec148b33873e88a397f51c92
parkyejin <uiop5809@naver.com>
2024-10-14 21:56:42 +0900

    [BOJ] 보물섬 / 골드5

---
+++ Baekjoon/BFS/2589 보물섬.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+string arr[51];
+int dist[51][51];
+
+const int dx[] = {1, 0, -1, 0};
+const int dy[] = {0, -1, 0, 1};
+
+int bfs(int x, int y){
+  memset(dist, 0, sizeof(dist));
+  queue <pair <int, int>> q;
+  q.push({x, y});
+  dist[x][y] = 1;
+
+  while(!q.empty()) {
+    int x = q.front().first;
+    int y = q.front().second;
+    q.pop();
+
+    for(int i = 0; i < 4; i++){
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+
+      if (nx < 0 || nx > N - 1 || ny < 0 || ny > M - 1) continue;
+      if (!dist[nx][ny] && arr[nx][ny] == 'L') {
+        q.push({nx, ny});
+        dist[nx][ny] = dist[x][y] + 1;
+      }
+    }
+  }
+
+  int res = 0;
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      res = max(res, dist[i][j] - 1);
+    }
+  }
+  return res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  cin >> N >> M;
+  for(int i = 0; i < N; i++){
+    cin >> arr[i];
+  }
+
+  int res = 0;
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      if (arr[i][j] == 'L') {
+        res = max(res, bfs(i, j));
+      }
+    }
+  }
+  cout << res;
+  return 0;
+} 
